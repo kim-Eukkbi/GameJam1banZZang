c8b14557c430ae0defe5e781f120e770fffd774d
김성후 <smbaekr56@naver.com>
2021-07-26 22:42:20 +0900

    뒤에 속도 만들었음

--- Assets/02.Scripts/PlayerInput.cs
+++ Assets/02.Scripts/PlayerInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using DG.Tweening;
 using System.Linq;
 
@@ -11,10 +12,13 @@ public class PlayerInput : MonoBehaviour
     private Rigidbody playerRigidbody;
     private ConstantForce playerConstantForce;
 
+    public TextMeshPro speedTMP;
     public List<Material> colorMat;
     private int colorindex;
     private RaycastHit hit;
     private PizzaType playerType;
+    private Vector3 tMPPos;
+    private bool isShaking = false;
 
     void Start()
     {
@@ -24,11 +28,36 @@ public class PlayerInput : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
         playerConstantForce = GetComponent<ConstantForce>();
         playerMesh.material = colorMat[(int)playerType];
+        tMPPos = speedTMP.rectTransform.position;
     }
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        float Valocity = Mathf.Abs(playerRigidbody.velocity.y);
+
+        if (Valocity > 200)
+        {
+            if (!isShaking)
+            {
+                isShaking = true;
+                speedTMP.transform.DOShakePosition(1, 5).OnComplete(() => isShaking = false);
+            }
+            speedTMP.text = string.Format("{0:0}", Valocity);
+            speedTMP.fontSize = Valocity > 800 ? 800 : Valocity * 2f;
+            speedTMP.rectTransform.position += new Vector3(0,  -(Valocity / 500), Valocity / 300);
+        }
+        else
+        {
+            speedTMP.text = " ";
+            speedTMP.fontSize = 400;
+            speedTMP.rectTransform.position = new Vector3(tMPPos.x, tMPPos.y + Camera.main.transform.position.y - 150, tMPPos.z);
+        }
+
+
+
+        
+
+        if (Input.GetMouseButtonDown(0))
         {
             if (playerType.Equals(PizzaType.GREEN))
                 playerType = PizzaType.RED;
@@ -41,8 +70,8 @@ public class PlayerInput : MonoBehaviour
         }
 
 
-        Debug.DrawRay(transform.position, Vector3.down, Color.blue, 6 + playerRigidbody.velocity.y / 100);
-        if(Physics.Raycast(transform.position,Vector3.down,out hit, 6 + playerRigidbody.velocity.y / 100))
+        Debug.DrawRay(transform.position, Vector3.down, Color.blue, 15 + playerRigidbody.velocity.y / 100);
+        if(Physics.Raycast(transform.position,Vector3.down,out hit, 15 + playerRigidbody.velocity.y / 100))
         {
             if(hit.transform.GetComponent<Pizza>().Type.Equals(playerType))
             {
