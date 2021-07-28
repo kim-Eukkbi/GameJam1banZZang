92c06023ede3ec5a82a48ef1549b9fe6873a3039
김성후 <smbaekr56@naver.com>
2021-07-28 10:15:17 +0900

    지금 처음 시작하는 에니메이션 추가 그리고 콤보 디테일 만드는중

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -22,10 +22,14 @@ public class Player : MonoBehaviour
     public Vector3 tMPPos;
     private bool isShaking = false;
     public bool isDoubleChack = false;
+    public bool isfadeEnd = false;
 
     void Start()
     {
-        //vCams[0].gameObject.SetActive(true);
+        speedTMP.gameObject.transform.localScale = Vector3.zero;
+        GameManager.instance.textCombo.gameObject.transform.localScale = Vector3.zero;
+        StartCoroutine(Fading());
+
         playerType = PizzaType.RED;
         playerMesh = GetComponent<MeshRenderer>();
         playerTrail = GetComponent<TrailRenderer>();
@@ -55,6 +59,11 @@ public class Player : MonoBehaviour
         float colorB = Mathf.Clamp(Valocity / 100, 0.43f, 1);
         speedTMP.color = new Color(colorR, colorG, colorB);
         speedTMP.fontSize = Valocity > 120 ? 500 : Valocity * 2f + 300;
+        float comboColorR = Mathf.Clamp(GameManager.instance.combo / 5, 0.17f, 1);
+        float comboColorG = Mathf.Clamp(GameManager.instance.combo / 5, 0.19f, 1);
+        float comboColorB = Mathf.Clamp(GameManager.instance.combo / 5, 0.43f, 1);
+        GameManager.instance.textCombo.color = new Color(comboColorR, comboColorG, comboColorB);
+
         //speedTMP.rectTransform.position += new Vector3(0, -(Valocity / 500), Valocity / 300);
 
         if (Input.GetMouseButtonDown(0))
@@ -112,10 +121,12 @@ public class Player : MonoBehaviour
         plate.DestroyPizza();
 
         yield return new WaitForSeconds(1f);
+    }
 
-        // for (int i = 0; i < plate.pizzas.Count; i++)
-        // {
-        //     plate.pizzas.RemoveAt(i);
-        // }
+    private IEnumerator Fading()
+    {
+        yield return new WaitForSeconds(1.5f);
+        speedTMP.gameObject.transform.DOScale(new Vector3(1.4647f, 1.3907f, 1), .5f);
+        GameManager.instance.textCombo.gameObject.transform.DOScale(new Vector3(1.4647f, 1.3907f, 1), .5f);
     }
 }
