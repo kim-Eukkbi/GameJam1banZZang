5d76ec729f184959bd4a26e3c5d5e461c1fff602
김성후 <smbaekr56@naver.com>
2021-07-28 10:36:31 +0900

    콤보 Anime 추가

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -61,10 +61,11 @@ public class Player : MonoBehaviour
         float colorB = Mathf.Clamp(Valocity / 100, 0.43f, 1);
         speedTMP.color = new Color(colorR, colorG, colorB);
         speedTMP.fontSize = Valocity > 120 ? 500 : Valocity * 2f + 300;
-        float comboColorR = Mathf.Clamp(GameManager.instance.combo / 5, 0.17f, 1);
-        float comboColorG = Mathf.Clamp(GameManager.instance.combo / 5, 0.19f, 1);
-        float comboColorB = Mathf.Clamp(GameManager.instance.combo / 5, 0.43f, 1);
-        GameManager.instance.textCombo.color = new Color(comboColorR, comboColorG, comboColorB);
+        GameManager.instance.textCombo.color = new Color(colorR, colorG, colorB);
+        GameManager.instance.textCombo.text = 
+            string.Format("{0:0}\n<size=50%>combo</size>", GameManager.instance.combo);
+        GameManager.instance.textCombo.fontSize = (Valocity > 120 ? 300 : Valocity * 2f);
+        GameManager.instance.textCombo.fontSize = Mathf.Clamp(GameManager.instance.textCombo.fontSize, 200, 300);
 
         //speedTMP.rectTransform.position += new Vector3(0, -(Valocity / 500), Valocity / 300);
 
@@ -98,6 +99,7 @@ public class Player : MonoBehaviour
                         currPlate.isChecking = true;
 
                         GameManager.instance.combo++;
+                        GameManager.instance.textCombo.transform.DOShakePosition(1, 5f);
                     }
                 }
                 else
@@ -113,7 +115,7 @@ public class Player : MonoBehaviour
                     }
                 }
 
-                GameManager.instance.textCombo.text = GameManager.instance.combo + "\n<size=200>combo</size>";
+                GameManager.instance.textCombo.text = GameManager.instance.combo + "\n<size=50%>combo</size>";
             }
         }
 
