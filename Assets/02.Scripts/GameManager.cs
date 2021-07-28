fe80e54cb91f27e6c7bb9f9feb456ff7ff83d090
김성후 <smbaekr56@naver.com>
2021-07-28 11:51:54 +0900

    모형탑 훈련 가즈아

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -256,7 +256,7 @@ public class GameManager : MonoBehaviour
         
 
         Vector3 pos = new Vector3(player.transform.position.x, player.transform.position.y - 150, player.transform.position.z);
-        Vector3 pos2 = new Vector3(player.transform.position.x + 12.5f, player.transform.position.y + 20f, player.transform.position.z);
+        Vector3 pos2 = new Vector3(player.transform.position.x, player.transform.position.y + 150f, player.transform.position.z);
 
         floor = Instantiate(gameOverFloor, pos, Quaternion.identity);
 
